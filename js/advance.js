02c9721efa26eba83e71a34722d264f219d02849
labietelabiete <victorhugogote@gmail.com>
2021-03-29 14:12:42 +0200

    Second commit

--- js/advance.js
+++ js/advance.js
@@ -1,6 +1,7 @@
 setInterval(function(){
     let matrixBoard = document.querySelectorAll('.board-item');
     let oldTarget = document.querySelector('.target-item');
+    let clickHereText = document.getElementById('click-here-text');
 
     /*Setting new random position for target element*/
     let randRow = Math.floor(Math.random() * 5) + 1;
@@ -8,11 +9,13 @@ setInterval(function(){
     let randPos = randRow.toString()+randColumn.toString();
 
     /*Setting new random size for target element*/
-    let randHeight = (10*(Math.floor(Math.random() * 5) + 5));
+    let randHeight = 10*(Math.floor(Math.random() * 5) + 5);
     let randWidth = 10*(Math.floor(Math.random() * 5) + 5);
 
     /*Removing target element style for current element*/
-    oldTarget.classList.remove("target-item")
+    oldTarget.classList.remove("target-item");
+    oldTarget.innerHTML = '';
+    // oldTarget.removeChild(oldTarget.lastElementChild);
 
     /*Adding target element to the new random element*/
     for (let i = 0; i < matrixBoard.length; i++) {
@@ -22,6 +25,7 @@ setInterval(function(){
             matrixBoard[i].classList.add("target-item")
             matrixBoard[i].style.height = randHeight.toString()+"%";
             matrixBoard[i].style.width = randWidth.toString()+"%";
+            matrixBoard[i].innerHTML = '<p id="click-here-text">Click here</p>'
         }
     }
      }, 1000);
