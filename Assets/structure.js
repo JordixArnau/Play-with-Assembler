3693303a5aec73a3bd4740379fa438e1a9c58715
Jordi <aejordi@gmail.com>
2021-03-29 12:05:56 +0200

    Script to include the templates added

--- Assets/structure.js
+++ Assets/structure.js
@@ -1,4 +1,7 @@
 function start() {
     let startTemp = document.getElementById('start');
+    document.querySelector('aside').style.display = 'none';
     document.querySelector('main').appendChild(startTemp);
 }
+
+start();
