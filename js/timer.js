97c4d4a04781fa67282ab2174cda387842d387ca
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-03-30 15:40:56 +0200

    countdown implimented

--- js/timer.js
+++ js/timer.js
@@ -1,15 +1,17 @@
-let startButton =  document.querySelector(".start-button");
-startButton.addEventListener('click', gameTimer)
+let countDown = document.querySelector("#timerCount");
+countDown.style.display = "flex";
+let maxTime = 10;
+let intervalValue = maxTime;
 
-function gameTimer() {
-startButton.style.display = "none";
-phase1();
-setTimeout(function (){
-    clickButton.removeEventListener('click', addToNumber);
-    
-    setTimeout(() => {
+let intervalCountdown = setInterval(function () {
+    countDown.innerHTML = intervalValue.toString();
+    intervalValue--;
+    if (intervalValue == -2) {
+        countDown.style.display = "none";
         finish();
-    }, 2000);
-}, 10000);
-}
+    }
+},1000);
+
+countDown.innerHTML = "10";
+
 
