3b26fedabd8b01102504da547164d1d2b5a9666b
Jordi <aejordi@gmail.com>
2021-04-06 14:27:15 +0200

    Reset and keyboard mouse mode integrated

--- js/formulary-eventlistener.js
+++ js/formulary-eventlistener.js
@@ -5,8 +5,7 @@ document.getElementById("name__player").addEventListener('keyup', checkName);
 document.getElementById("fase1__btn").addEventListener('click', checkMode);
 document.getElementById("fase2__btn").addEventListener('click', checkMode);
 
-document.getElementById("low__level__btn").addEventListener('click', checkDifficulty);
-document.getElementById("medium__level__btn").addEventListener('click', checkDifficulty);
-document.getElementById("hard__level__btn").addEventListener('click', checkDifficulty);
+document.getElementById("mouse_btn").addEventListener('click', checkDifficulty);
+document.getElementById("keyboard_btn").addEventListener('click', checkDifficulty);
 
 document.getElementById("start__btn").addEventListener('click', toReady);
